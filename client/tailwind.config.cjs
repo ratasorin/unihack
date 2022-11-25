23bc4cec47bd9907408a725373b307b486221eb5
Radu <radutek13@gmail.com>
2022-11-25 23:11:26 +0200

    Added tailwindcss

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navbar1: '#40A36E',
+        navbar2: '#A5C268',
+      },
+    },
+  },
+  plugins: [],
+}
