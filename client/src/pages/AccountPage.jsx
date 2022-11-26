eb7a9f857e626611657cdf97a950efb4a1a49d26
AndyStan112 <stanica.andrei@gmail.com>
2022-11-26 18:13:33 +0200

    finished signup

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,44 +1,36 @@
-import { useState } from "react";
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import Button from '../components/controls/button';
 const AccountPage = () => {
-    return (
+  return (
+    <form action="http://localhost:3000/api/signUp" method="post">
+      <label>
+        Name:
+        <input type="text" id="mail" name="mail" placeholder="Your mail here" />
+      </label>
 
-        <form>
-            <label>
-                Name: 
-                <input
-                 type="text"
-                 id="name"
-                 name="name"
-                 placeholder="Your Name here"
-                />
-            </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="*********"
+        />
+      </label>
 
-            <label>
-                Password: 
-                <input
-                 type="text"
-                 id="password"
-                 name="password"
-                 placeholder="*********"
-                />
-            </label>
+      <label>
+        Confirm password:
+        <input
+          type="password"
+          id="password2"
+          name="password2"
+          placeholder="*********"
+        />
+      </label>
 
-            <label>
-                Confirm password: 
-                <input
-                 type="text"
-                 id="password2"
-                 name="password2"
-                 placeholder="*********"
-                />
-            </label>
-
-            <Button>Submit</Button>
-        </form>
-        
-    )
-
-  };
+      <Button>Submit</Button>
+    </form>
+  );
+};
 export default AccountPage;
