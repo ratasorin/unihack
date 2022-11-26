1704261ef7f54d58b65c6b0b3590a21b424c8521
Radu <radutek13@gmail.com>
2022-11-26 15:35:15 +0200

    Planner update

--- client/src/pages/PlannerPage.jsx
+++ client/src/pages/PlannerPage.jsx
@@ -1,7 +1,7 @@
 /**************************
  * IMPORTS
  ***************************/
-
+import Timetable from '../components/timetable';
 
 /**************************
  * CONFIG
@@ -13,6 +13,7 @@ const PlannerPage = () => {
   return (
     <>
       <h1 className='text-3xl p-2 border-0 border-b-1 border-dotted border-gray-400'>Plan de repetiție</h1>
+      <Timetable />
     </>
   )
 };
