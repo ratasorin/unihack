c4821742264f5eda987761065c4e4bf568a18370
Radu <radutek13@gmail.com>
2022-11-26 12:19:06 +0200

    Added custom colors for button

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -9,6 +9,8 @@ module.exports = {
         navbar1: '#40A36E',
         navbar2: '#A5C268',
         navbarOpen: 'rgba(255,255,255,50%)',
+        btngreen1: '#88E9AF',
+        btngreen2: '#DDD370',
       },
     },
   },
