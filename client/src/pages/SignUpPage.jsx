ebd99514f9e9f70f81eac2a127f972a89ec63bf2
Radu <radutek13@gmail.com>
2022-11-27 00:19:31 +0200

    Sign up changes

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -8,32 +8,32 @@ export default function SignUpPage() {
     <div id='bodyBackground' style={{ backgroundImage: 'url(/src/assets/green-pattern.jpg)' }}></div>
     <div className='mx-auto backdrop-blur-sm w-96 m-6 p-4 bg-white-alpha-80 rounded-lg shadow-lg shadow-black-alpha-50 flex flex-col gap-y-4'>
       <h1 className='text-2xl text-center'>Sign Up</h1>
-      <form 
+      <form
         id='signupForm'
-        action='http://localhost:3000/api/signUp' 
+        action='http://localhost:3000/api/signUp'
         method='POST'
         className='flex flex-col gap-y-2'
       >
         <label>
           Email:
           <Textbox
-              type='email'
-              required
-              id='mail'
-              name='mail'
-              className='block w-full'
-              placeholder='youremail@example.com'
+            type='email'
+            required
+            id='mail'
+            name='mail'
+            className='block w-full'
+            placeholder='youremail@example.com'
           />
         </label>
         <label>
-            Password:
-            <Textbox
-                type='password'
-                required
-                id='password'
-                name='password'
-                className='block w-full'
-            />
+          Password:
+          <Textbox
+            type='password'
+            required
+            id='password'
+            name='password'
+            className='block w-full'
+          />
         </label>
         <label>
             Confirm Password:
