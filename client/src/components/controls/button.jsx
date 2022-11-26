4483c7bb5dc54b9e33e88be99bc0a2003dd8991e
Radu <radutek13@gmail.com>
2022-11-26 12:19:45 +0200

    Added stylized button

--- client/src/components/controls/button.jsx
+++ client/src/components/controls/button.jsx
@@ -1,5 +1,15 @@
-const Button = (props) => {
-    return (
-        <button>{props.content}</button>
-    );
+export function Button(props) {
+    if (props.color == 'green') {
+        return (
+            <button className="px-1.5 py-0.5 rounded-md bg-gradient-to-r from-btngreen1 to-btngreen2 transition-all hover:brightness-105 active:brightness-95">{props.children}</button>
+        );
+    } else if (props.color == 'red') {
+        return (
+            <button className="px-1.5 py-0.5 rounded-md bg-gradient-to-r from-rose-500 to-purple-400 text-white transition-all hover:brightness-105 active:brightness-95">{props.children}</button>
+        );
+    } else {
+        return (
+            <button className="px-1.5 py-0.5 rounded-md bg-gradient-to-r  from-slate-300 to-sky-100 transition-all hover:brightness-105 active:brightness-95">{props.children}</button>
+        );
+    }
 }
