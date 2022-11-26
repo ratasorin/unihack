4b45a2b9990c62a8063ffb0e538a587b6122add0
Radu <radutek13@gmail.com>
2022-11-26 12:23:33 +0200

    Added enabled prop

--- client/src/components/controls/textbox.jsx
+++ client/src/components/controls/textbox.jsx
@@ -1,5 +1,5 @@
 export function Textbox(props) {
     return (
-        <input type={props.type} onChange={props.onChange} id={props.id} name={props.name} value={props.value} placeholder={props.placeholder} className='border-0 border-b-2 px-1.5 pt-0.5 bg-slate-100 rounded-md border-slate-400' />
+        <input type={props.type} onChange={props.onChange} id={props.id} name={props.name} value={props.value} enabled={props.enabled} placeholder={props.placeholder} className='border-0 border-b-2 px-1.5 pt-0.5 bg-slate-100 rounded-md border-slate-400' />
     )
 }
