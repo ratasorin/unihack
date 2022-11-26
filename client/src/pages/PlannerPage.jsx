9ab18879c8444340ddffc608bffd5730a6189a6e
Radu <radutek13@gmail.com>
2022-11-26 14:27:57 +0200

    Broken Planner

--- client/src/pages/PlannerPage.jsx
+++ client/src/pages/PlannerPage.jsx
@@ -1,12 +1,23 @@
 /**************************
  * IMPORTS
  ***************************/
+import { LineChart, Label, CartesianGrid, XAxis, YAxis } from 'recharts';
+
 
 /**************************
  * CONFIG
  ***************************/
 const PlannerPage = () => {
-  return <h1 className='text-2xl'>Plan de repetiție</h1>;
+  return (
+    <>
+      <h1 className='text-3xl mt-6 border-0 border-b-1 border-dotted border-gray-400'>Plan de repetiție</h1>
+      <LineChart width={500} height={400} className="mx-auto my-6">
+        <CartesianGrid stroke="#ccc" />
+        <XAxis />
+        <YAxis />
+      </LineChart>
+    </>
+  )
 };
 
 export default PlannerPage;
