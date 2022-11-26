fb22de6419e62b24f6bfdf7aae41c2aec488adff
Radu <radutek13@gmail.com>
2022-11-26 20:14:17 +0200

    Updated textbox to use custom CSS

--- client/src/components/controls/textbox.jsx
+++ client/src/components/controls/textbox.jsx
@@ -1,6 +1,8 @@
+import './textbox.css';
+
 export default function Textbox(props) {
   return (
     <input type={props.type} onChange={props.onChange} required={props.required} id={props.id} name={props.name} value={props.value} enabled={props.enabled} placeholder={props.placeholder} 
-      className='border-0 border-b-2 px-1.5 pt-0.5 bg-slate-100 rounded-md border-slate-400 shadow-sm' />
+      className='textbox' />
   )
 }
