f825bbd9824632720e2861dc68eb90506eb1a3d6
Radu <radutek13@gmail.com>
2022-11-26 21:49:31 +0200

    Improved routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,40 @@
 /*******************************
  * IMPORTS
  *******************************/
-// router
-import { Routes, Route } from "react-router-dom";
+/*******************************
+ * COMPONENTS
+ *******************************/
 
-// components
-import { Navbar, Navlink } from "./components/navbar";
-import { Page } from './components/page';
-import { Icon } from "@iconify/react";
-
-// pages
-import HomePage from "./pages/HomePage";
-import PlannerPage from "./pages/PlannerPage";
-import PracticePage from "./pages/PracticePage";
-import AccountPage from "./pages/AccountPage";
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 /*******************************
- * CONFIG
+ * PAGES
+ *******************************/
+import Layout from './pages/Layout';
+import HomePage from './pages/HomePage';
+import PlannerPage from './pages/PlannerPage';
+import PracticePage from './pages/PracticePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+
+/*******************************
+ * COM
  *******************************/
 function App() {
   return (
-    <>
-      <Navbar>
-        <Navlink href="/practice">Practice</Navlink>
-        <Navlink href="/planner">Planner</Navlink>
-        <Navlink href="/account">
-          <Icon
-            icon="ic:baseline-person-outline"
-            className="inline-block align-top"
-            width="24"
-          />
-          Account
-        </Navlink>
-      </Navbar>
-      <Page>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/planner" element={<PlannerPage />}></Route>
-          <Route path="/practice" element={<PracticePage />}></Route>
-          <Route path="/account" element={<AccountPage />}></Route>
-        </Routes>
-      </Page>
-    </>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path='planner' element={<PlannerPage />} />
+        <Route path='practice' element={<PracticePage />} />
+        <Route path='account'>
+          <Route index element={<Navigate to='login' />} />
+          <Route path='login' element={<LoginPage />} />
+          <Route path='signup' element={<SignUpPage />} />
+        </Route>
+        {/* <Route path='*' element={<NotFoundPage />} /> */}
+      </Route>
+    </Routes>
   );
 }
 
