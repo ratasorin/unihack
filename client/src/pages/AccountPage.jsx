f68abdbe7b2d269e46543e71b44958cd8de4c54f
RaduTek <52799099+RaduTek@users.noreply.github.com>
2022-11-27 14:04:14 +0200

    Removed centralized background

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -10,12 +10,6 @@ import ProgressCard from "../components/progressCard";
 export default function AccountPage() {
 	return (
 		<>
-			<div
-				id="bodyBackground"
-				style={{
-					backgroundImage: "url(/src/assets/green-pattern.jpg)",
-				}}
-			></div>
 			<main className="mx-auto max-w-xl mt-12 bg-white-alpha-80 backdrop-blur-sm rounded-md shadow-md p-4 gap-2 shadow-black-alpha-20 flex flex-col flex-wrap justify-center items-center">
 				<h1 className="text-xl">Welcome to your account!</h1>
 				<div className="flex gap-2">
