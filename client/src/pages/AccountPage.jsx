9e19b54e53ab7e91c54bcf777245c2984922b08d
RaduTek <52799099+RaduTek@users.noreply.github.com>
2022-11-27 14:27:41 +0200

    Update AccountPage

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -13,12 +13,12 @@ export default function AccountPage() {
 			<main className="mx-auto max-w-xl mt-12 bg-white-alpha-80 backdrop-blur-sm rounded-md shadow-md p-4 gap-2 shadow-black-alpha-20 flex flex-col flex-wrap justify-center items-center">
 				<h1 className="text-xl">Welcome to your account!</h1>
 				<div className="flex gap-2">
-					<ProgressCard name="Eseu 1" val="50%" />
-					<ProgressCard name="Eseu 2" val="35%" />
+					<ProgressCard name="Europe" val="50%" />
+					<ProgressCard name="Mathematics - Calculus" val="35%" />
 				</div>
 				<div className="flex gap-2">
-					<ProgressCard name="Eseu 3" val="0%" />
-					<ProgressCard name="Eseu 4" val="66%" />
+					<ProgressCard name="Computer Science" val="10%" />
+					<ProgressCard name="Mathematics - Algebra" val="66%" />
 				</div>
 				<p>
 					<Link to="/account/logout" className="btn btn-red">
