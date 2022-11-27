78c12d0a3d910e189575d629b71b731aced57155
Alex Tunea <alextuneaa@gmail.com>
2022-11-27 12:43:28 +0200

    Added background to first and second stages

--- client/src/components/practice-options/FirstStage.jsx
+++ client/src/components/practice-options/FirstStage.jsx
@@ -19,34 +19,40 @@ const FirstStage = (params) => {
   }
 
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-xl font-bold mb-4">How well do you want to master your subject?</h1>
-      <label>
+    <>
+      <div
+        id="bodyBackground"
+        style={{ backgroundImage: 'url(/src/assets/green-pattern.jpg)' }}
+      ></div>
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-bold mb-4">How well do you want to master your subject?</h1>
+        <label>
+          <input 
+            type="checkbox"
+            value={checkedOne}
+            onChange={handleChangeOne}
+          />
+          <span>Only the basics</span> 
+        </label>
+        <label>
         <input 
-          type="checkbox"
-          value={checkedOne}
-          onChange={handleChangeOne}
-        />
-        <span>Only the basics</span> 
-      </label>
-      <label>
-      <input 
-          type="checkbox"
-          value={checkedTwo}
-          onChange={handleChangeTwo}
-        />
-        <span>Intermediate</span> 
-      </label>
-      <label>
-      <input 
-          type="checkbox"
-          value={checkedThree}
-          onChange={handleChangeThree}
-        />
-        <span>God tier knowledge</span> 
-      </label>
-    <Button onClick={() => {params.setStage(2)}}>Next stage</Button>
-    </div>
+            type="checkbox"
+            value={checkedTwo}
+            onChange={handleChangeTwo}
+          />
+          <span>Intermediate</span> 
+        </label>
+        <label>
+        <input 
+            type="checkbox"
+            value={checkedThree}
+            onChange={handleChangeThree}
+          />
+          <span>God tier knowledge</span> 
+        </label>
+      <Button onClick={() => {params.setStage(2)}}>Next stage</Button>
+      </div>
+    </>
   );
 };
 
