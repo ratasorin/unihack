b2c8b5236b5ca85aaed6d9ac55d6a68d38709361
RaduTek <52799099+RaduTek@users.noreply.github.com>
2022-11-27 12:41:35 +0200

    Added tbody

--- client/src/components/timetable/index.jsx
+++ client/src/components/timetable/index.jsx
@@ -9,28 +9,30 @@ import TTEvent from './ttevent';
  ***************************/
 export default function Timetable(params) {
 
-    var prev = 0;
+  var prev = 0;
 
-    return (
-        <div className='timetable-wrap'>
-        <table className='timetable'>
-            {params.data.tasks.map(task => <tr>
-                {!(prev = 0) && ''}
-                <th>{task[0]}</th>
-                <td></td>
-                {task[1].map(evt => <>
-                    {[...Array(evt[0]-prev-1)].map(i => (<td></td>))}
-                    {(prev += evt[0]) && ''}
-                    <td><TTEvent popup={evt.length == 3 && evt[2]} width='150%'>{evt[1]}</TTEvent></td>
-                </>)}
-            </tr>)}
-            <tr>
-                <th></th>
-                <th></th>
-                {params.data.dates.map(date => <th>{date}</th>)}
-                <th></th>
-            </tr>
-        </table>
-        </div>
-    );
+  return (
+    <div className='timetable-wrap'>
+      <table className='timetable'>
+        <tbody>
+          {params.data.tasks.map(task => <tr>
+            {!(prev = 0) && ''}
+            <th>{task[0]}</th>
+            <td></td>
+            {task[1].map(evt => <>
+              {[...Array(evt[0] - prev - 1)].map(i => (<td></td>))}
+              {(prev += evt[0]) && ''}
+              <td><TTEvent popup={evt.length == 3 && evt[2]} width='150%'>{evt[1]}</TTEvent></td>
+            </>)}
+          </tr>)}
+          <tr>
+            <th></th>
+            <th></th>
+            {params.data.dates.map(date => <th>{date}</th>)}
+            <th></th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
