5401ca5b8a402393e9195bc5251965132cb84d86
RaduTek <52799099+RaduTek@users.noreply.github.com>
2022-11-27 11:53:28 +0200

    Undo CORS fix

--- server/pages/api/signIn.ts
+++ server/pages/api/signIn.ts
@@ -1,56 +1,49 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
-import argon2 from 'argon2';
-import prisma from '~prisma/index';
-import dotenv from 'dotenv';
+import type { NextApiRequest, NextApiResponse } from "next";
+import argon2 from "argon2";
+import prisma from "~prisma/index";
+import dotenv from "dotenv";
 
 dotenv.config();
 type Success = {
-  sessionId: string;
+	sessionId: string;
 };
 
 type Error = {
-  error: string;
+	error: string;
 };
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Success | Error>,
+	req: NextApiRequest,
+	res: NextApiResponse<Success | Error>
 ) {
-  res.setHeader('Access-Control-Request-Method', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Request-Headers', '*');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Max-Age', '3600');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+	const password = await prisma.password.findFirst({
+		where: { User: { mail: req.body.mail } },
+	});
+	const user = await prisma.user.findFirst({
+		where: { mail: req.body.mail },
+	});
 
-  const password = await prisma.password.findFirst({
-    where: { User: { mail: req.body.mail } },
-  });
-  const user = await prisma.user.findFirst({
-    where: { mail: req.body.mail },
-  });
+	if (!user) return res.status(500).json({ error: "Account does not exist" });
 
-  if (!user) return res.status(500).json({ error: 'Account does not exist' });
+	if (!password)
+		return res.status(500).json({ error: "Password does not exist" });
 
-  if (!password)
-    return res.status(500).json({ error: 'Password does not exist' });
+	const { hash, salt } = password;
 
-  const { hash, salt } = password;
+	if (!argon2.verify(hash, req.body.password + salt + process.env.pepper))
+		return res.status(500).json({ error: "Password does not match" });
 
-  if (!argon2.verify(hash, req.body.password + salt + process.env.pepper))
-    return res.status(500).json({ error: 'Password does not match' });
-
-  try {
-    const { id: sessionId } = await prisma.session.create({
-      data: {
-        status: 'active',
-        userId: user.id,
-      },
-      select: { id: true },
-    });
-    res.status(200).json({ sessionId });
-  } catch (error) {
-    res.status(500).json({ error: 'There was an unexpected error !' });
-  }
+	try {
+		const { id: sessionId } = await prisma.session.create({
+			data: {
+				status: "active",
+				userId: user.id,
+			},
+			select: { id: true },
+		});
+		res.status(200).json({ sessionId });
+	} catch (error) {
+		res.status(500).json({ error: "There was an unexpected error !" });
+	}
 }
--- server/pages/api/signOut.ts
+++ server/pages/api/signOut.ts
@@ -1,35 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '~prisma/index';
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "~prisma/index";
 
 type Success = {
-  sessionId: string;
+	sessionId: string;
 };
 
 type Error = {
-  error: string;
+	error: string;
 };
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Success | Error>,
+	req: NextApiRequest,
+	res: NextApiResponse<Success | Error>
 ) {
-  res.setHeader('Access-Control-Request-Method', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Request-Headers', '*');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Max-Age', '3600');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-
-  try {
-    await prisma.session.update({
-      where: { id: req.body.sessionId },
-      data: {
-        status: 'expired',
-      },
-    });
-    res.status(200);
-  } catch (error) {
-    res.status(500).json({ error: 'There was an unexpected error !' });
-  }
+	try {
+		await prisma.session.update({
+			where: { id: req.body.sessionId },
+			data: {
+				status: "expired",
+			},
+		});
+		res.status(200);
+	} catch (error) {
+		res.status(500).json({ error: "There was an unexpected error !" });
+	}
 }
