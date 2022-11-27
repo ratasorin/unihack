cda05bcbe965e2b5c6780ac6817e49e3a113fca8
Alex Tunea <alextuneaa@gmail.com>
2022-11-27 11:50:38 +0200

    Updated Practice Page

--- client/src/pages/PracticePage.jsx
+++ client/src/pages/PracticePage.jsx
@@ -18,17 +18,17 @@ const PracticePage = () => {
   let cardText;
 
   if (stage === 1) {
-    cardText = <FirstStage />;
+    cardText = <FirstStage setStage={setStage} />;
   } else if (stage === 2) {
-    cardText = <SecondStage />;
+    cardText = <SecondStage setStage={setStage} />;
   } else {
-    cardText = <ThirdStage />;
+    cardText = <ThirdStage setStage={setStage} />;
   }
 
   return (
     <main className="h-main flex flex-wrap flex-col items-center">
-      <h1 className="text-4xl bold mt-12 font-bold">Start praticing!</h1>
-      <div className="h-80 w-4/5 bg-cardColor rounded-3xl mt-16">
+      <h1 className="text-4xl bold mt-12 font-bold text-center">Help us help you customize your learning experience!</h1>
+      <div className="h-80 w-4/5 bg-cardColor shadow-lg shadow-black-alpha-20 rounded-3xl mt-16">
         <div className="m-8">{cardText}</div>
       </div>
     </main>
