b0e1424750e434bb63ac3bf777c14c953fbc4d70
RaduTek <52799099+RaduTek@users.noreply.github.com>
2022-11-27 14:04:04 +0200

    Added third stage

--- client/src/components/practice-options/ThirdStage.jsx
+++ client/src/components/practice-options/ThirdStage.jsx
@@ -1,5 +1,25 @@
-const ThirdStage = () => {
-  return <h1>Third stage</h1>;
-};
+import * as React from "react";
+import Button from "../controls/button";
+import { Icon } from "@iconify/react";
 
-export default ThirdStage;
+export default function ThirdStage(params) {
+	return (
+		<>
+			<div className="flex flex-col items-center gap-3">
+				<h1 className="text-2xl font-bold text-center">
+					3. Ready to practice?
+				</h1>
+				<p>We saved your work and prepared exercises from it.</p>
+				<div className="flex flex-col items-end justify-center mr-4">
+					<Button
+						onClick={() => {
+							params.setStage(3);
+						}}
+					>
+						Start practicing!
+					</Button>
+				</div>
+			</div>
+		</>
+	);
+}
