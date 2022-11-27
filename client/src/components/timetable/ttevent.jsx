f9dd824e9144ef0f2f7756a53243ace5ec922ef5
RaduTek <52799099+RaduTek@users.noreply.github.com>
2022-11-27 13:13:47 +0200

    Update timetable event

--- client/src/components/timetable/ttevent.jsx
+++ client/src/components/timetable/ttevent.jsx
@@ -9,23 +9,23 @@ import { Icon } from '@iconify/react';
  * COMPONENT
  ***************************/
 export default function TTEvent(params) {
-    const [popupVisible, setPopupVisible] = useState(false);
+  const [popupVisible, setPopupVisible] = useState(false);
 
-    const togglePopup = () => {
-        setPopupVisible(!popupVisible);
-    }
+  const togglePopup = () => {
+    setPopupVisible(!popupVisible);
+  }
 
 
 
-    return (
-        <div className='ttevent'>
-            <button className='ttcard' onClick={togglePopup} style={{width: params.width, backgroundColor: params.color}}>
-                {params.children}
-            </button>
-            {(popupVisible && params.popup != '') && (<div className='ttpopup'>
-                {params.popup}
-                <button className='ttclose' onClick={() => setPopupVisible(false)}><Icon icon='ic:baseline-close' /></button>
-            </div>)}
-        </div>
-    )
+  return (
+    <div className='ttevent'>
+      <button className='ttcard' onClick={togglePopup} style={{ width: params.width, backgroundColor: params.color }}>
+        {params.children}
+      </button>
+      {(popupVisible && params.popup != '') && (<div className='ttpopup'>
+        {params.popup}
+        <button className='ttclose' onClick={() => setPopupVisible(false)}><Icon icon='ic:baseline-close' /></button>
+      </div>)}
+    </div>
+  )
 }
