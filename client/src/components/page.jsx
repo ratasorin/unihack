ac70ca641521516cfe56d8b5bff0539ca44ddc12
RaduTek <52799099+RaduTek@users.noreply.github.com>
2022-11-27 14:27:36 +0200

    Update page

--- client/src/components/page.jsx
+++ client/src/components/page.jsx
@@ -1,6 +1,11 @@
 export function Page(props) {
-  return (
-    // Centered page
-    <div className="max-w-5xl mx-auto pt-20">{props.children}</div>
-  );
+	return (
+		// Centered page
+		<div
+			className="max-w-5xl mx-auto pt-20"
+			style={{ minHeight: "calc(100vh - 200px)" }}
+		>
+			{props.children}
+		</div>
+	);
 }
